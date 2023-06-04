cba092abbb9d1e27626ac2099313cfbf3c52ccba
WillBarriosC <will.barrios.89@gmail.com>
2023-06-04 10:50:55 -0500

    changes-v2.1

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import img from "/public/img-header.jpg";
 
 export default function Header({ scrollHandler }) {
   return (
-    
     <header className="relative">
       <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
       <div className="mx-auto">
@@ -12,15 +11,13 @@ export default function Header({ scrollHandler }) {
             <Image
               priority
               fill
-              className="h-full w-full"
+              className="h-full w-full object-cover"
               src={img}
               placeholder="blur"
               alt="Cover"
             />
-            <div className="absolute inset-0 bg-orange-100 mix-blend-multiply" />
           </div>
           <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
-            
             <h1 className="mt-4 text-center font-bold uppercase  sm:text-5xl sm:tracking-tight lg:text-7xl">
               <span className="block text-white">
                 Life is most healthy with
@@ -40,6 +37,5 @@ export default function Header({ scrollHandler }) {
         </div>
       </div>
     </header>
-    
   );
 }
